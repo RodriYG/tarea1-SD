5985960d7f0d3ca07b4a1d6c3dcea32a35bd2f3e
Rodrigo Yañez <rodrigoye90@gmail.com>
2024-04-20 19:22:49 -0400

    gRPC with Redis working, but only with a single query (code)

--- cache-clasico/gRPC-cliente/index.js
+++ cache-clasico/gRPC-cliente/index.js
@@ -2,6 +2,16 @@ const path = require('path');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const express = require('express');
+const redis = require('redis');
+
+const clientRedis = redis.createClient({
+    host: '0.0.0.0',
+    port: 6379
+});
+
+clientRedis.on('error', err => console.log('Redis Client Error', err));
+
+clientRedis.connect();
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../protos/servicios.proto'));
 
@@ -13,7 +23,7 @@ const app = express();
 app.listen(3000);
 console.log(`Listening on port 3000`)
 
-app.get('/airports', (req, res) => {
+app.get('/airports', async (req, res) => {
     console.log('Backend');
     client.getAll({}, (error, response) => {
         if (error) {
@@ -26,13 +36,20 @@ app.get('/airports', (req, res) => {
 });
 
 
-app.get('/airports/:code', (req, res) => {
+app.get('/airports/:code', async (req, res) => {
+    const cache =  await clientRedis.get(req.params.code);
+    if (cache) {
+        console.log('Cache');
+        res.json(JSON.parse(cache));
+        return;
+    }
     console.log('Backend');
     client.getAirport({ code: req.params.code }, (error, response) => {
         if (error) {
             res.status(500).json({ error: error.details });
         }
         else {
+            clientRedis.set(req.params.code, JSON.stringify(response));
             res.json(response);
         }
     });
