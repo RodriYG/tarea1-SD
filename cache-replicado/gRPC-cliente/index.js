eb8be2c89f15b9b34159d05aebd16ab7e143c37e
Rodrigo Yañez <rodrigoye90@gmail.com>
2024-04-20 21:06:00 -0400

    Redis replication working

---
+++ cache-replicado/gRPC-cliente/index.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const express = require('express');
+const redis = require('redis');
+const {client1,client2,client3} = require('./redis.js');
+
+
+
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../protos/servicios.proto'));
+
+const proto_path = grpc.loadPackageDefinition(packageDefinition);
+
+const client = new proto_path.Airports('0.0.0.0:50051', grpc.credentials.createInsecure());
+
+const app = express();
+app.listen(3000);
+console.log(`Listening on port 3000`)
+
+app.get('/airports', async (req, res) => {
+    console.log('Backend');
+    client.getAll({}, (error, response) => {
+        if (error) {
+            res.status(500).json({ error: error.details });
+        }
+        else {
+            res.json(response);
+        }
+    });
+});
+
+
+app.get('/airports/:code', async (req, res) => {
+    const cache =  await client1.get(req.params.code);
+    if (cache) {
+        console.log('Cache');
+        res.json(JSON.parse(cache));
+        return;
+    }
+    console.log('Backend');
+    client.getAirport({ code: req.params.code }, (error, response) => {
+        if (error) {
+            res.status(500).json({ error: error.details });
+        }
+        else {
+            client1.set(req.params.code, JSON.stringify(response));
+            res.json(response);
+        }
+    });
+});
---
+++ cache-replicado/gRPC-servidor/index.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const pg = require('pg');
+
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../protos/servicios.proto'));
+const proto_path = grpc.loadPackageDefinition(packageDefinition);
+
+
+const pool = new pg.Pool({
+    user: 'user',
+    host: '0.0.0.0',
+    database: 'tarea1',
+    password: 'user',
+    port: 5432,
+});
+
+function findAirport(call, callback) {
+    pool.query('SELECT * FROM AIRPORTS WHERE code = $1', [call.request.code], (error, results) => {
+        if (error) {
+            callback({
+                message: 'Error occurred',
+                code: grpc.status.INTERNAL
+            });
+        }
+        else if (results.rows.length > 0) {
+            callback(null, results.rows[0]);
+        }
+        else {
+            callback({
+                message: 'Degree not found',
+                code: grpc.status.INVALID_ARGUMENT
+            });
+        }
+    });
+}
+
+function getAllAirports(call, callback) {
+    pool.query('SELECT * FROM AIRPORTS', (error, results) => {
+        if (error) {
+            console.log(error);
+            callback({
+                message: 'Error occurred',
+                code: grpc.status.INTERNAL
+            });
+        }
+        else {
+            callback(null, { airports: results });
+        }
+    });
+}
+
+const server = new grpc.Server();
+
+server.addService(proto_path.Airports.service, { getAirport: findAirport, getAll: getAllAirports});
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+});
